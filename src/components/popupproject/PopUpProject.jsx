b4b5a04d13b5a0456dc75e988663a229aef43377
ThousandAllen <wdwocnj@gmail.com>
2022-01-14 22:14:01 -0800

    Started working on pop-up page for projects, this is the jsx file for the gneral popup

---
+++ src/components/popupproject/PopUpProject.jsx
@@ -0,0 +1,10 @@
+import Popup from 'reactjs-popup'
+import 'reactjs-popup/dist/index.css'
+
+export default function PopUpProject() {
+    return (
+        <div className="popupproject">
+            
+        </div>
+    )
+}
